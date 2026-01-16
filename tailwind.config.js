8432b851bdad311883a3d39b689e93933644936e
Phuc Cao <phuccao@mia.vn>
2026-01-16 22:15:23 +0700

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 module.exports = {
-  content: [
-    './public/index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  darkMode: "class",
+  content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: (theme) => ({
+      colors: {
+        primary: flattenColorPalette(theme("colors.blue")),
+        secondary: flattenColorPalette(theme("colors.gray")),
+      },
+    }),
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require("@tailwindcss/forms")],
 };
